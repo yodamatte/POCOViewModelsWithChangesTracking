7cd06f26795235fbd3cc33001582e394a746db05
yodamatte <yodamatte@outlook.com>
2023-10-02 22:37:38 +0200

    Add project files.

---
+++ PocoViewModel.cs
@@ -0,0 +1,37 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+
+namespace POCOViewModelsWithChangesTracking
+{
+    [POCOViewModel]
+    public class PocoViewModel : ViewModelBase
+    {
+        public ChangeTracker ChangeTracker { get; set; }
+        public virtual string Name { get; set; }
+        public virtual bool IsChanged { get; protected set; }
+        public static PocoViewModel Create()
+        {
+           return ViewModelSource.Create(() => new PocoViewModel());
+        }
+
+        protected PocoViewModel() 
+        {
+            Name = string.Empty;
+            ChangeTracker = new(this);
+            ChangeTracker.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(ChangeTracker.IsChanged))
+                {
+                    IsChanged = ChangeTracker.IsChanged;
+                }
+            };
+        }
+
+        [Command]
+        public void AcceptChanges() 
+        {
+            ChangeTracker.AcceptChanges();
+        }
+    }
+}
